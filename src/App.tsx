ab2d8d42f6abca41a6c0ace21e36f0af8777a2fd
Janella <fullmoonfirefly11@gmail.com>
2024-03-15 16:12:23 -0400

    Add data display

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Button, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Grid } from '@mui/material'
+import { Button, Grid, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material'
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import './App.css'
 
+import villagersJSON from './assets/data/villagers.json' // import villagers data
+
 export function Header() {
   return (
-    <Grid id='nav-bar' container spacing={3} justifyContent="center" alignItems="center" direction="row">
-      <Grid item xs={9}>
+    <Grid id='nav-bar' container spacing={2}>
+      <Grid item xs={11} >
         <h1 id='nav-bar-h1'>Stardew Gifts Finder</h1>
       </Grid>
       <Grid item>
@@ -18,22 +20,65 @@ export function Header() {
   )
 }
 
-export function Data() {
+function createDataCell(items: string[] | null) {
+  const cells = []
+  if (items != null) {
+    for (const i of items) {
+      cells.push(
+        <li>{i}</li>
+      )
+    }
+  }
+
   return (
-    <TableContainer>
-      <Table>
+    <TableCell size='small'>
+      {cells}
+    </TableCell>
+  )
+}
+
+function createDataRow() {
+  const rows = []
+  for (const villager of villagersJSON) {
+    if (villager.name != "") {
+      rows.push(
+        <TableRow key={villager.name}>
+          <TableCell>
+            <h2 style={{margin:'0'}}>
+              {villager.name}
+            </h2>
+            <span>
+              {villager.birthday}
+            </span>
+          </TableCell>
+          {createDataCell(villager.loves)}
+          {createDataCell(villager.likes)}
+          {createDataCell(villager.neutral)}
+          {createDataCell(villager.dislikes)}
+          {createDataCell(villager.loves)}
+        </TableRow>
+      )
+    }
+  }
+  return (rows)
+}
+
+export function DisplayData() {
+  return (
+    <TableContainer sx={{width:'99%', alignContent:'center', margin:1}} component={Paper}>
+      <Table size='small' stickyHeader>
         <TableHead>
           <TableRow>
-            <TableCell>Villager</TableCell>
-            <TableCell>Loves</TableCell>
-            <TableCell>Likes</TableCell>
-            <TableCell>Neutral</TableCell>
-            <TableCell>Dislikes</TableCell>
-            <TableCell>Hates</TableCell>
+            <TableCell variant='head'>Villager</TableCell>
+            <TableCell variant='head'>Loves</TableCell>
+            <TableCell variant='head'>Likes</TableCell>
+            <TableCell variant='head'>Neutral</TableCell>
+            <TableCell variant='head'>Dislikes</TableCell>
+            <TableCell variant='head'>Hates</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          
+          {createDataRow()}
         </TableBody>
       </Table>
     </TableContainer>
@@ -43,8 +88,14 @@ export function Data() {
 export default function Home() {
   return(
     <>
-      <Header />
-      <Data />
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item xs={12}>
+          <Header />
+        </Grid>
+        <Grid item xs={12}>
+          <DisplayData />
+        </Grid>
+      </Grid>
     </>
   )
 }
